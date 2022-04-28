8b9e9b70fed08aa46b59dbb6cabe782c533be020
Geoff Horowitz <geoffhorowitz@gmail.com>
2022-04-28 23:27:51 +0000

    fixed error in accuracy function

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
- def accuracy(output, target):
+def accuracy(output, target):
      """Computes the precision@k for the specified values of k"""
      batch_size = target.shape[0]
      _, pred = torch.max(output, dim=-1)
